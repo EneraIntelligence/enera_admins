595bff0146ed1d06ae748ac4e25a526d2898e878
Alfonso Rodriguez <alfonso@vizinova.com>
2016-02-12 12:36:18 -0600

    preview middleware added to the project

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -10,9 +10,6 @@
     <!-- Remove Tap Highlight on Windows Phone IE -->
     <meta name="msapplication-tap-highlight" content="no"/>
     <link rel="icon" type="image/png" href="{!! URL::asset('images/favicon.png') !!}" sizes="32x32">
-    {{--<link rel="icon" type="image/png" href="assets/img/favicon-16x16.png" sizes="16x16">
-    <link rel="icon" type="image/png" href="assets/img/favicon-32x32.png" sizes="32x32">--}}
-    <link rel="icon" type="image/png" href="{!! URL::asset('img/favicon.png') !!}" sizes="32x32">
     <title>Enera Admins @yield('title')</title>
     @yield('head_scripts')
 
