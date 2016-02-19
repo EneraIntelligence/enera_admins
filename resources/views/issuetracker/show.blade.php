8accd4a50d4d93a6489ae2bfb85ce99830767c3c
Pedro Luna <pluna@enera.mx>
2016-02-18 22:24:28 -0600

    [Enhacement] IssueTrackerController > index testing

--- resources/views/issuetracker/show.blade.php
+++ resources/views/issuetracker/show.blade.php
@@ -218,6 +218,7 @@
             $(".issue-data-expand").click(function () {
                 var $this = $(this);
                 $("#" + $this.attr('data-pre')).slideToggle("slow", function () {
+                    console.log($this.find('i'));
                     $this.find('i').html($this.find('i').html() == '&#xE5CF;' ? '&#xE5CE;' : '&#xE5CF;');
                 });
             });
