36f439cad754f858fff988318a8042536662d93b
Pedro Luna <pluna@enera.mx>
2016-02-18 23:01:46 -0600

    [Enhacement] IssueTrackerController > index

--- resources/views/issuetracker/show.blade.php
+++ resources/views/issuetracker/show.blade.php
@@ -52,7 +52,7 @@
                         <div class="uk-width-medium-3-4">
                             <div class="uk-margin-large-bottom">
                                 <h2 class="heading_c uk-margin-small-bottom">Contexto (
-                                    Linea: {{ $issue->file['line'] }})</h2>
+                                    L: {{ $issue->file['line'] }})</h2>
                                 <pre data-start="{!! $issue->file['line'] > 9 ? $issue->file['line'] - 9 : 1 !!}"
                                      class="line-numbers" style="max-height: 380px; !important;">
                                     <code class="language-php"
