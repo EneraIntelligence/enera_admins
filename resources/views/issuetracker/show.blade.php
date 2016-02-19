1766dcf6fd1acf3bfb585c69ed9b5fa722ef1e8a
Pedro Luna <pluna@enera.mx>
2016-02-18 20:25:49 -0600

    [Enhacement] IssueTrackerController > index testing

--- resources/views/issuetracker/show.blade.php
+++ resources/views/issuetracker/show.blade.php
@@ -55,7 +55,7 @@
                             </div>
                             <div class="uk-margin-large-bottom">
                                 <h2 class="heading_c uk-margin-small-bottom">URL</h2>
-                                <pre class="line-numbers">{!! '\\n'.$issue->url.'\\n' !!}</pre>
+                                <pre class="line-numbers">{!! "\\n".$issue->url."\\n" !!}</pre>
                             </div>
                             <div class="uk-margin-large-bottom">
                                 <h2 class="heading_c uk-margin-small-bottom">Variables de Sesión</h2>
