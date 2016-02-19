06abe30e16caf27c7dc7d42614ab377d5cbd15d6
Pedro Luna <pluna@enera.mx>
2016-02-18 20:07:24 -0600

    [Enhacement] IssueTrackerController > index testing

--- resources/views/issuetracker/show.blade.php
+++ resources/views/issuetracker/show.blade.php
@@ -65,6 +65,13 @@
                                 </pre>
                             </div>
                             <div class="uk-margin-large-bottom">
+                                <h2 class="heading_c uk-margin-small-bottom">Variables de Sesión</h2>
+                                <pre id="tracer" class="line-numbers">
+                                    <code class="language-php"
+                                          style="margin-top: -20px;">{!! $issue->exception['trace'] !!}</code>
+                                </pre>
+                            </div>
+                            <div class="uk-margin-large-bottom">
                                 <h2 class="heading_c uk-margin-small-bottom">Comentarios</h2>
                                 <ul class="uk-comment-list">
                                     <li>
