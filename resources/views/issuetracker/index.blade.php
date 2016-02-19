4e4810d0e645a12901a7fe52472644c6140458ea
Pedro Luna <pluna@enera.mx>
2016-02-19 13:22:24 -0600

    [Enhacement] IssueTrackerController > index, ancho de columnas

--- resources/views/issuetracker/index.blade.php
+++ resources/views/issuetracker/index.blade.php
@@ -35,7 +35,7 @@
                                id="ts_issues">
                             <thead>
                             <tr>
-                                <th width="30%">Issue</th>
+                                <th width="20%">Issue</th>
                                 <th>Responsable</th>
                                 <th>Prioridad</th>
                                 <th>Fecha</th>
