5347990cc22d986cb23eadab5851860bb225f3c3
Pedro Luna <pluna@enera.mx>
2016-02-23 13:57:02 -0600

    [FIX] IssueTrackerHelper

--- resources/views/issuetracker/show.blade.php
+++ resources/views/issuetracker/show.blade.php
@@ -64,7 +64,7 @@
                             </div>
                             <div class="uk-margin-large-bottom">
                                 <h2 class="heading_c uk-margin-small-bottom">URL</h2>
-                                <pre><a href="#">{{ $issue->request['url'] }}</a></pre>
+                                <p><a href="#">{{ $issue->request['url'] }}</a></p>
                             </div>
                             <div class="uk-margin-large-bottom">
                                 <h2 class="heading_c uk-margin-small-bottom issue-data-expand" data-pre="session_vars">
