63a5ca05b89f4460cf528219f4dbf6687c2470bb
Alfonso Rodriguez <alfonso@vizinova.com>
2016-02-29 17:29:06 -0600

    changes in client view

--- resources/views/admin/clients/index.blade.php
+++ resources/views/admin/clients/index.blade.php
@@ -5,13 +5,13 @@
 @endsection
 @section('content')
     <div id="page_heading" data-uk-sticky="{ top: 48, media: 960 }">
-        <div class="heading_actions">
-            <form action="{!! route('admin::clients::search') !!}" method="post"
+
+        <!-- <form action="{!! route('admin::clients::search') !!}" method="post"
                   class="uk-form">
                 <div class="uk-grid">
                     <div class="uk-width-medium-2-3">
                         <div class="md-input-wrapper"><label for="contact_list_search">Encontrar
-                                usuario</label><input class="md-input" type="text" size="60"
+                                usuario</label><input class="md-input" type="text"
                                                       id="contact_list_search"
                                                       name="search"><span
                                     class="md-input-bar"></span></div>
@@ -42,10 +42,56 @@
                         <li><a href="#">Other Action</a></li>
                     </ul>
                 </div>
+            </div> -->
+        <div class="uk-grid">
+            <div class="uk-width-medium-2-10">
+                <h1>Lista de Clietes</h1>
             </div>
+            <div class="uk-width-medium-6-10">
+                <form action="{!! route('admin::clients::search') !!}" method="post"
+                      class="uk-form">
+                    <div class="uk-grid">
+                        <div class="uk-width-medium-5-6">
+                            <div class="md-input-wrapper"><input class="md-input" type="text"
+                                                          id="contact_list_search"
+                                                          name="search"><span
+                                        class="md-input-bar"></span></div>
+                        </div>
+                        <div class="uk-width-medium-1-6 uk-hidden-small">
+                            <button type="submit" class="header_main_search_btn uk-button-link"><i
+                                        class="md-icon material-icons">&#xE8B6;</i>
+                            </button>
+                        </div>
+                    </div>
+                    <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+                </form>
+            </div>
+            <div class="uk-width-medium-2-10 uk-hidden-small">
+                <a href="#" data-uk-tooltip="{pos:'bottom'}" title="Archive"><i
+                            class="md-icon material-icons">
+                        &#xE149;</i></a>
+                <a href="#" data-uk-tooltip="{pos:'bottom'}" title="Print"><i class="md-icon material-icons">
+                        &#xE8AD;</i></a>
+                <div class="uk-button-dropdown" data-uk-dropdown>
+
+                    <!-- This is the button toggling the dropdown -->
+                    <i class="md-icon material-icons">&#xE5D4;</i>
+
+                    <!-- This is the dropdown -->
+                    <div class="uk-dropdown uk-dropdown-small">
+                        <ul class="uk-nav uk-nav-dropdown">
+                            <li><a href="">Action</a></li>
+                            <li><a href="">Other Action</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
-        <h1>Lista de Clietes</h1>
     </div>
+
+
     {{-- <div id="top_bar">
          <div class="md-top-bar">
              <div class="uk-width-large-8-10 uk-container-center">
@@ -69,9 +115,8 @@
         <div id="page_content_inner">
 
             <div class="md-card-list-wrapper" id="mailbox">
-                <div class="uk-width-large-8-10 uk-container-center">
+                <div class="uk-width-large-1 uk-container-center">
                     <div class="md-card-list">
-                        <div class="md-card-list-header heading_list">Clientes</div>
                         <div class="md-card-list-header md-card-list-header-combined heading_list"
                              style="display: none">All Messages
                         </div>
