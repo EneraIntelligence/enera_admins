da087170345880454f92617e984d4aa87cca3a29
Alfonso Rodriguez <alfonso@vizinova.com>
2016-02-29 12:45:13 -0600

    changes in view - search input relocated

--- resources/views/admin/clients/index.blade.php
+++ resources/views/admin/clients/index.blade.php
@@ -4,8 +4,48 @@
     {!! HTML::style(asset('assets/css/campaign.css')) !!}
 @endsection
 @section('content')
-
-    <h3 style="margin: 10px 40px;" class="heading_b uk-margin-bottom">Pricing Tables</h3>
+    <div id="page_heading" data-uk-sticky="{ top: 48, media: 960 }">
+        <div class="heading_actions">
+            <form action="{!! route('admin::clients::search') !!}" method="post"
+                  class="uk-form">
+                <div class="uk-grid">
+                    <div class="uk-width-medium-2-3">
+                        <div class="md-input-wrapper"><label for="contact_list_search">Encontrar
+                                usuario</label><input class="md-input" type="text" size="60"
+                                                      id="contact_list_search"
+                                                      name="search"><span
+                                    class="md-input-bar"></span></div>
+                    </div>
+                    <div class="uk-width-medium-1-3">
+                        <button type="submit" class="header_main_search_btn uk-button-link"><i
+                                    class="md-icon material-icons">&#xE8B6;</i>
+                        </button>
+                    </div>
+                </div>
+                <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+            </form>
+            <a href="#" data-uk-tooltip="{pos:'bottom'}" title="Archive"><i class="md-icon material-icons"
+                                                                            style="position: relative;
+  transform: translateY(-40%);">
+                    &#xE149;</i></a>
+            <a href="#" data-uk-tooltip="{pos:'bottom'}" title="Print"><i class="md-icon material-icons"
+                                                                          style="position: relative;
+  transform: translateY(-40%);">
+                    &#xE8AD;</i></a>
+            <div data-uk-dropdown style="position: relative;
+  transform: translateY(-40%);">
+                <i class="md-icon material-icons">&#xE5D4;</i>
+                <div class="uk-dropdown uk-dropdown-small">
+                    <ul class="uk-nav">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Other Action</a></li>
+                        <li><a href="#">Other Action</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <h1>Lista de Clietes</h1>
+    </div>
     {{-- <div id="top_bar">
          <div class="md-top-bar">
              <div class="uk-width-large-8-10 uk-container-center">
@@ -30,45 +70,8 @@
 
             <div class="md-card-list-wrapper" id="mailbox">
                 <div class="uk-width-large-8-10 uk-container-center">
-
-                    <div class="md-card uk-margin-medium-bottom">
-                        <div class="md-card-content">
-                            <div class="uk-grid" data-uk-grid-margin="">
-                                <div class="uk-width-medium-1-2 uk-row-first">
-                                    <div class="uk-vertical-align">
-                                        <div class="uk-vertical-align-middle">
-
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="uk-width-medium-1-2">
-                                    <form action="{!! route('admin::clients::search') !!}" method="post"
-                                          class="uk-form">
-                                        <div class="uk-grid">
-                                            <div class="uk-width-medium-2-3">
-                                                <div class="md-input-wrapper"><label for="contact_list_search">Encontrar
-                                                        usuario</label><input class="md-input" type="text"
-                                                                              id="contact_list_search"
-                                                                              name="search"><span
-                                                            class="md-input-bar"></span></div>
-                                            </div>
-                                            <div class="uk-width-medium-1-3">
-                                                    <button type="submit" class="header_main_search_btn uk-button-link"><i
-                                                                class="md-icon material-icons">&#xE8B6;</i>
-                                                    </button>
-                                            </div>
-                                        </div>
-
-
-                                        <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                     <div class="md-card-list">
-                        <div class="md-card-list-header heading_list">Today</div>
+                        <div class="md-card-list-header heading_list">Clientes</div>
                         <div class="md-card-list-header md-card-list-header-combined heading_list"
                              style="display: none">All Messages
                         </div>
@@ -80,8 +83,8 @@
                                     <div class="md-card-list-item-select">
                                         <input type="checkbox" data-md-icheck/>
                                     </div>
-                                    <div class="md-card-list-item-avatar-wrapper">
-                                        <span class="md-card-list-item-avatar md-bg-grey">hp</span>
+                                    <div class="md-card-list-item-avatar-wrapper"><span
+                                                class="md-card-list-item-avatar md-bg-grey">hp</span>
                                     </div>
                                     <div class="md-card-list-item-sender">
                                         <span>{{$client->name}}</span>
