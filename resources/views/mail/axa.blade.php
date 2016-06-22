7a74e326829a592ac10c27ec91b18d197b165d53
Alfonso Rodriguez <darkdreke@gmail.com>
2016-06-22 11:29:16 -0500

    email view for axa

---
+++ resources/views/mail/axa.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <link rel="stylesheet" href="">
+</head>
+<body style="padding:0; margin:0; background-color:#eaeaea;">
+<div style="width: 100%;">
+    <div style="width: 25%;  display: inline-block;" class="hide"></div>
+    <div style="width: 50%; min-width: 360px; margin: 0 auto; display: inline-block;" class="mobile">
+        <div style="width: 100%;  display: inline-block;">
+            <a href="http://actualmanager.com/click.php?aspid=b27cae35dc6c8d946379ee61c7532dcd" target="_blank"><img
+                        src="http://axa-hogarprotegido.com/newsletters/12/images/axa-hogar-08_01.jpg" width="100%"
+                        style="display:block; border:0;" alt="Axa Hogar"/></a>
+        </div>
+        <div style="width: 100%;  display: inline-block; background-color:#ffffff; top: -5px; position: relative;">
+            <p style="text-align: center; font-family:Arial, Helvetica, sans-serif; font-size:35px; color:#103084; text-decoration:none;">
+                <strong>Protege tu Hogar de<br>
+                    Cualquier Da&ntilde;o o Robo </strong></p>
+            <p style="text-align: center;">
+                <span style="font-family:Arial, Helvetica, sans-serif; font-size:16px; color:#333333; text-decoration:none;">Con </span><span
+                        style="font-family:Arial, Helvetica, sans-serif; font-size:16px; color:#103084; text-decoration:none;"><strong>Hogar
+                        Integral</strong></span><span
+                        style="font-family:Arial, Helvetica, sans-serif; font-size:16px; color:#333333; text-decoration:none;"> eliges la cobertura que prefieres para </span><span
+                        style="font-family:Arial, Helvetica, sans-serif; font-size:16px; color:#103084; text-decoration:none;"><strong>cuidar
+                        tu patrimonio</strong></span><span
+                        style="font-family:Arial, Helvetica, sans-serif; font-size:16px; color:#333333; text-decoration:none;">.<br>
+              Adem&aacute;s lo estar&aacute;s asegurando de cualquier accidente, robo, incendio o explosi&oacute;n.</span>
+            </p>
+            <p style="text-align: center;"><span
+                        style="font-family:Arial, Helvetica, sans-serif; font-size:16px; color:#333333; text-decoration:none;"> conoce </span><span
+                        style="font-family:Arial, Helvetica, sans-serif; font-size:16px; color:#e7050e; text-decoration:none;"><strong>nuestras
+                        coberturas</strong></span><span
+                        style="font-family:Arial, Helvetica, sans-serif; font-size:16px; color:#333333; text-decoration:none;"> aqu&iacute;:</span>
+            </p>
+            <a href="http://actualmanager.com/click.php?aspid=b27cae35dc6c8d946379ee61c7532dcd" target="_blank"
+               style="margin: 20px auto ;width: 150px;text-align: center; padding: 15px;background-color:#e7050e; font-family:Arial, Helvetica, sans-serif; font-size:23px; color:#f9f9f9; display:block; text-decoration:none; line-height:40px;"><strong>Saber
+                    m&aacute;s &raquo;</strong></a>
+            <img src="http://axa-hogarprotegido.com/newsletters/12/images/axa-hogar-09_02.jpg" width="100%" style=" display:block; border:0;"  alt="coberturas Hogar Integral"/>
+            <img src="http://axa-hogarprotegido.com/newsletters/12/images/axa-hogar-09_03.jpg" width="100%" style=" display:block; border:0;"  alt="Axa"/>
+        </div>
+    </div>
+    <div style="width: 24%;  display: inline-block;" class="hide"></div>
+</div>
+</body>
+</html>
