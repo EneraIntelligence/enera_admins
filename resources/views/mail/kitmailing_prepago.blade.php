ffab85f1b43b786df965fd28786221d49f510a2c
Alfonso Rodriguez <darkdreke@gmail.com>
2016-07-11 15:57:18 -0500

    created email for kitmailign prepago test 2

--- resources/views/mail/kitmailing_prepago.blade.php
+++ resources/views/mail/kitmailing_prepago.blade.php
@@ -8,35 +8,45 @@
     <link rel="stylesheet" href="">
 </head>
 <body style="padding:0; margin:0; background-color:#eaeaea;">
-<div style="margin: 0 auto; max-width: 575px;">
-    <table cellpadding="0" cellspacing="0" align="center" style="width:100% !important">
+<div>
+    <table cellpadding="0" cellspacing="0" align="center" style="width:100%; max-width: 575px;">
         <tr>
             <td>
-                <a href="http://interacttrk.com/?a=21744&c=20792&s1="><img src="http://7458fef244112da9ec42-3a7a28bc54f1e977a943ce68ab4c0361.r33.cf2.rackcdn.com/april/movistar1.jpg" style="display:block;border:0px;" width="100%" height="100%" alt="Movistar"></a>
+                <a href="http://interacttrk.com/?a=21744&c=20792&s1="><img
+                            src="http://7458fef244112da9ec42-3a7a28bc54f1e977a943ce68ab4c0361.r33.cf2.rackcdn.com/april/movistar1.jpg"
+                            style="display:block;border:0px; width: 100%; height: 83px; max-width: 575%" alt="Movistar"></a>
             </td>
         </tr>
         <tr height="150">
             <td bgcolor="#005c84" align="center">
-                <a href="http://interacttrk.com/?a=21744&c=20792&s1=" style="font-family:arial;font-size:24px;color:white;text-decoration:none;"><span style="font-size:36px;">&iexcl;No busques m&aacute;s! Tenemos<br>la mejor tarifa de m&oacute;vil para ti</span></a>
+                <a href="http://interacttrk.com/?a=21744&c=20792&s1="
+                   style="font-family:arial;font-size:24px;color:white;text-decoration:none;"><span
+                            style="font-size:36px;">&iexcl;No busques m&aacute;s! Tenemos<br>la mejor tarifa de m&oacute;vil para ti</span></a>
             </td>
         </tr>
         <tr>
             <td>
-                <a href="http://interacttrk.com/?a=21744&c=20792&s1="><img src="http://7458fef244112da9ec42-3a7a28bc54f1e977a943ce68ab4c0361.r33.cf2.rackcdn.com/april/movistar2.jpg" style="display:block;border:0px;" width="100%" height="100%" alt=""></a>
+                <a href="http://interacttrk.com/?a=21744&c=20792&s1="><img
+                            src="http://7458fef244112da9ec42-3a7a28bc54f1e977a943ce68ab4c0361.r33.cf2.rackcdn.com/april/movistar2.jpg"
+                            style="display:block;border:0px; width: 100%; height: 222px; max-width: 575%"" alt=""></a>
             </td>
         </tr>
         <tr>
             <td>
-                <a href="http://interacttrk.com/?a=21744&c=20792&s1="><img src="http://7458fef244112da9ec42-3a7a28bc54f1e977a943ce68ab4c0361.r33.cf2.rackcdn.com/april/movistar3.jpg" style="display:block;border:0px;" width="100%" height="100%" alt="Movistar"></a>
+                <a href="http://interacttrk.com/?a=21744&c=20792&s1="><img
+                            src="http://7458fef244112da9ec42-3a7a28bc54f1e977a943ce68ab4c0361.r33.cf2.rackcdn.com/april/movistar3.jpg"
+                            style="display:block;border:0px; width: 100%; height: 222px; max-width: 575%"" alt="Movistar"></a>
             </td>
         </tr>
         <tr height="150">
             <td bgcolor="#479d06" align="center">
-                <a href="http://interacttrk.com/?a=21744&c=20792&s1=" style="font-family:arial;font-size:44px;color:white;text-decoration:none;"><strong><span style="font-size:32px;">Aprovecha la promoci&oacute;n</span><br>&iexcl;INF&Oacute;RMATE AQU&Iacute;!</strong></a>
+                <a href="http://interacttrk.com/?a=21744&c=20792&s1="
+                   style="font-family:arial;font-size:44px;color:white;text-decoration:none;"><strong><span
+                                style="font-size:32px;">Aprovecha la promoci&oacute;n</span><br>&iexcl;INF&Oacute;RMATE
+                        AQU&Iacute;!</strong></a>
             </td>
         </tr>
     </table>
-
 </div>
 </body>
 </html>
