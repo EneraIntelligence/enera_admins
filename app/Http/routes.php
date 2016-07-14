4d891581d7c378e910d617126921881c06d1cafe
Alfonso Rodriguez <darkdreke@gmail.com>
2016-07-14 16:12:12 -0500

    create mail fom enera_promass

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -86,7 +86,7 @@ Route::get('/choose', ['as' => 'choose.platform', function () {
 
 //route for tests of emails
 Route::get('/test-email', function () {
-    return view('mail.kitmailing_prepago');
+    return view('mail.enera_promass');
 });
 /*Route::get('/test', function () {
     return view('massivemail.unsubscribeok');
---
+++ resources/views/mail/enera_promass.blade.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <link rel="stylesheet" href="">
+
+    <style type="text/css">
+        .ExternalClass {
+            width: 100%;
+        }
+
+        /* Esta imagen de adapta al ancho de lo que lo contenga con un m�ximo de 650px */
+        img, object, embed, video {
+            width: 100%;
+            max-width: 575px;
+        }
+
+        /* IE 6 does not support max-width so default to width 100% */
+        .ie6 img {
+            width: 100%;
+        }
+    </style>
+
+</head>
+<body style="padding:0; margin:0; background-color:#eaeaea;">
+<table width="100%" border="0" cellspacing="0" cellpadding="0" style="background-color:#eaeaea;">
+    <tr>
+        <td align="center" valign="top">
+            <table width="100%" style="max-width:652px; background-color:#FFF;" border="0" cellspacing="0"
+                   cellpadding="0">
+                <tr>
+                    <td style="background-color:#eaeaea;" align="center" valign="top">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td align="left" valign="top"></td>
+                </tr>
+                <tr>
+                    <td align="center" valign="middle" style="background-color:#ffffff;"><a
+                                href="http://actualmanager.com/click.php?aspid=199a82eecf505e7ca23b5bad1f0dd08c"
+                                target="_blank"><img
+                                    src="http://mass-seguros.com/newsletters/13/images/promass-afiliados-06_01.jpg"
+                                    alt="Promass" width="650" height="100%"
+                                    style="max-width:652px; display:block; border:0;"/></a></td>
+                </tr>
+
+
+                <tr>
+
+                    <td height="109" align="center" valign="bottom"
+                        style="font-family:Arial, Helvetica, sans-serif; font-size:36px; color:#192857; text-decoration:none;">
+                        <strong>Maneja tranquilo con un seguro<br> adecuado para tu auto</strong></td>
+
+                </tr>
+
+                <tr>
+                    <td style="background-color:#ffffff;" height="20" align="center"></td>
+                </tr>
+
+                <tr>
+                    <td align="center" valign="top" style="background-color:#ffffff;"><a
+                                href="http://actualmanager.com/click.php?aspid=199a82eecf505e7ca23b5bad1f0dd08c"
+                                target="_blank"><img
+                                    src="http://mass-seguros.com/newsletters/13/images/promass-afiliados-06_03.jpg"
+                                    alt="Maneja Tranquilo" width="650" height="100%"
+                                    style="max-width:652px; display:block; border:0;"/></a></td>
+                </tr>
+                <tr>
+                <tr>
+
+                    <td style="background-color:#ffffff;" align="center" valign="top">
+                        <table width="200" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td style="background-color:#f48021;" height="50" align="center" valign="middle"><a
+                                            href="http://actualmanager.com/click.php?aspid=199a82eecf505e7ca23b5bad1f0dd08c"
+                                            target="_blank"
+                                            style=" background-color:#f48021; font-family:Arial, Helvetica, sans-serif; font-size:22px; color:#f9f9f9; display:block; text-decoration:none; line-height:40px;"><strong>Cotiza
+                                            Ahora &raquo;</strong></a></td>
+                            </tr>
+
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="background-color:#ffffff;" height="30" align="center"></td>
+                </tr>
+
+                <tr>
+                    <td height="40" align="center" valign="top" style="background-color:#ffffff;">
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td width="12%">&nbsp;</td>
+                                <td style="font-family:Arial, Helvetica, sans-serif; font-size:18px; color:#192857; text-decoration:none;">
+                                    <strong>coberturas:</strong></td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+
+
+                <tr>
+                <tr>
+                    <td align="left" valign="top" style="background-color:#ffffff;">
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0" height="63">
+                            <tr>
+
+                                <td width="27%" align="right"><img
+                                            src="http://mass-seguros.com/newsletters/13/images/promass-afiliados-06_04.jpg">
+                                </td>
+                                <td width="24%" align="left" valign="middle"><span
+                                            style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#575757; text-decoration:none;"> <strong>Garant&iacute;a </strong>de servicio.</span>
+                                </td>
+                                <td width="19%" align="right"><img
+                                            src="http://mass-seguros.com/newsletters/13/images/promass-afiliados-06_06.jpg">
+                                </td>
+                                <td align="left" valign="middle"><span
+                                            style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#575757; text-decoration:none;"> <strong>Responsabilidad</strong> civil. </span>
+                                </td>
+                            </tr>
+
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="left" valign="top" style="background-color:#ffffff;">
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0" height="78">
+                            <tr>
+
+                                <td width="27%" align="right"><img
+                                            src="http://mass-seguros.com/newsletters/13/images/promass-afiliados-06_10.jpg">
+                                </td>
+                                <td width="24%" align="left" valign="middle"><span
+                                            style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#575757; text-decoration:none;"> <strong>Robo </strong>total.</span>
+                                </td>
+                                <td width="19%" align="right"><img
+                                            src="http://mass-seguros.com/newsletters/13/images/promass-afiliados-06_12.jpg">
+                                </td>
+                                <td align="left" valign="middle"><span
+                                            style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#575757; text-decoration:none;"> <strong>Asistencia
+                                            m&eacute;dica</strong> y legal. </span></td>
+                            </tr>
+
+                        </table>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align="left" valign="top" style="background-color:#ffffff;">
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0" height="81">
+                            <tr>
+
+                                <td width="27%" align="right"><img
+                                            src="http://mass-seguros.com/newsletters/13/images/promass-afiliados-06_14.jpg">
+                                </td>
+                                <td width="24%" align="left" valign="middle"><span
+                                            style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#575757; text-decoration:none;"> <strong>Da&ntilde;os </strong>materiales y<strong><br>
+                                            p&eacute;rdida total</strong>.</span></td>
+                                <td width="19%" align="right"><img
+                                            src="http://mass-seguros.com/newsletters/13/images/promass-afiliados-06_15.jpg">
+                                </td>
+                                <td align="left" valign="middle"><span
+                                            style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#575757; text-decoration:none;"> <strong>Seguimiento
+                                            en
+                                            l&iacute;nea</strong> a la reparaci&oacute;n de tu veh&iacute;culo.</span>
+                                </td>
+                            </tr>
+
+                        </table>
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td style="font-family:Arial, Helvetica, sans-serif; font-size:12px; color:#575757; text-decoration:none;"
+                        height="90" align="center">En caso de robo o p&eacute;rdida total durante el primer a&ntilde;o
+                        de uso del veh&iacute;culo&nbsp;<br>
+                        asegurado, te pagamos el valor de la factura.<br><br>
+                        <a href="http://actualmanager.com/click.php?aspid=199a82eecf505e7ca23b5bad1f0dd08c"
+                           target="_blank"
+                           style="font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#f4801f;"><strong>Saber
+                                m&aacute;s &raquo;</strong></a>
+                    </td>
+                </tr>
+                <tr height="25">
+                    <td bgcolor="#eaeaea" align="center">
+                        <p style="text-align: center; padding: 10px; color: darkgray;">Si ya no deseas recibir información haz
+                            {{--<a href="{!! route('unSubscribe') !!}/{!! $data['email'] !!}" style="text-decoration: none">clic--}}
+                                aquí.</a>
+                        </p>
+                    </td>
+                </tr>
+
+            </table>
+        </td>
+    </tr>
+
+</table>
+</td>
+</tr>
+</table>
+</body>
+</html>
