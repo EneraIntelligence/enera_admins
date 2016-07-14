ed33396aec56ab5616b4eee0ed2c0c8268db182c
Alfonso Rodriguez <darkdreke@gmail.com>
2016-07-14 17:04:58 -0500

    create mail for enera_aigseguros

---
+++ resources/views/mail/enera_aigseguros.blade.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <link rel="stylesheet" href="">
+</head>
+<table cellpadding="0" cellspacing="0" align="center" style="width:100%; max-width: 575px;">
+    <table width="100%" border="0" cellspacing="0" cellpadding="0" style="background-color:#f1f1f1;">
+        <tr>
+            <td align="center" valign="top">
+                <table width="100%" style="max-width:650px; background-color:#FFF;" border="0" cellspacing="0"
+                       cellpadding="0">
+                    <tr>
+                        <td style="background-color:#f1f1f1;" align="left" valign="top">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td align="left" valign="top"></td>
+                    </tr>
+                    <tr>
+                        <td align="left" valign="top" style="background-color:#00a4e4;">
+                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                <tr>
+                                    <td align="left" valign="top" width="5%">&nbsp;</td>
+                                    <td align="left" valign="top"><a
+                                                href="http://actualmanager.com/click.php?aspid=515512876488c17dccba70be83414ab7"
+                                                target="_blank"><img
+                                                    src="http://cotizar-seguro-auto.com/newsletters/19/images/aig-01.jpg"
+                                                    width="100%" style="max-width:90px; display:block; border:0;"
+                                                    alt="AIG"/></a></td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="left" valign="top" height="25">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td align="center" valign="top"
+                            style="font-family:Arial, Helvetica, sans-serif; font-size:31px; color:#e77b1c;">
+                            <strong>&iquest;Por qu&eacute; asegurar tu auto con nosotros?</strong></td>
+                    </tr>
+                    <tr>
+                        <td align="left" valign="top" height="25">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td align="left" valign="top">
+                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                <tr>
+                                    <td align="left" valign="top" width="3%">&nbsp;</td>
+                                    <td align="left" valign="top" width="10%"><a
+                                                href="http://actualmanager.com/click.php?aspid=515512876488c17dccba70be83414ab7"
+                                                target="_blank"><img
+                                                    src="http://cotizar-seguro-auto.com/newsletters/19/images/aig-02.jpg"
+                                                    width="100%" style="max-width:57px; display:block; border:0;"
+                                                    alt="Chofer"/></a></td>
+                                    <td align="left" valign="top" width="1%">&nbsp;</td>
+                                    <td align="left" valign="middle" width="36%"
+                                        style="font-family:Arial, Helvetica, sans-serif; color:#373737; font-size:14px;">
+                                        <strong style=" color:#00a4e4; font-size:17px;">No te quedes sin
+                                            auto</strong><br>
+                                        Ponemos un chofer a tu servicio
+                                    </td>
+                                    <td align="left" valign="top" width="2%">&nbsp;</td>
+                                    <td align="left" valign="top" width="10%"><a
+                                                href="http://actualmanager.com/click.php?aspid=515512876488c17dccba70be83414ab7"
+                                                target="_blank"><img
+                                                    src="http://cotizar-seguro-auto.com/newsletters/19/images/aig-05.jpg"
+                                                    width="100%" style="max-width:57px; display:block; border:0;"
+                                                    alt="Garant&iacute;a"/></a></td>
+                                    <td align="left" valign="top" width="1%">&nbsp;</td>
+                                    <td align="left" valign="middle" width="36%"
+                                        style="font-family:Arial, Helvetica, sans-serif; color:#373737; font-size:14px;">
+                                        <strong style=" color:#00a4e4; font-size:17px;">Garant&iacute;a de
+                                            servicio</strong><br>
+                                        Pago en m&aacute;ximo 72 horas*
+                                    </td>
+                                    <td align="left" valign="top" width="1%">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td align="left" valign="top" height="35">&nbsp;</td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="middle"
+                                        style="font-family:Arial, Helvetica, sans-serif; color:#373737; font-size:14px;">
+                                        &nbsp;</td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="middle"
+                                        style="font-family:Arial, Helvetica, sans-serif; color:#373737; font-size:14px;">
+                                        &nbsp;</td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="top"><a
+                                                href="http://actualmanager.com/click.php?aspid=515512876488c17dccba70be83414ab7"
+                                                target="_blank"><img
+                                                    src="http://cotizar-seguro-auto.com/newsletters/19/images/aig-03.jpg"
+                                                    width="100%" style="max-width:57px; display:block; border:0;"
+                                                    alt="P&eacute;rdida parcial/total"/></a></td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="middle"
+                                        style="font-family:Arial, Helvetica, sans-serif; color:#373737; font-size:14px;">
+                                        <strong style=" color:#00a4e4; font-size:17px;">P&eacute;rdida
+                                            total</strong><br>
+                                        Por da&ntilde;os o hurto
+                                    </td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="top"><a
+                                                href="http://actualmanager.com/click.php?aspid=515512876488c17dccba70be83414ab7"
+                                                target="_blank"><img
+                                                    src="http://cotizar-seguro-auto.com/newsletters/19/images/aig-06.jpg"
+                                                    width="100%" style="max-width:57px; display:block; border:0;"
+                                                    alt="Asistencia"/></a></td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="middle"
+                                        style="font-family:Arial, Helvetica, sans-serif; color:#373737; font-size:14px;">
+                                        <strong style=" color:#00a4e4; font-size:17px;">Asistencia</strong><br>
+                                        Cuenta con nuestras exclusivas
+                                        asistencias 24/7
+                                    </td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td align="left" valign="top" height="35">&nbsp;</td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="middle"
+                                        style="font-family:Arial, Helvetica, sans-serif; color:#373737; font-size:14px;">
+                                        &nbsp;</td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="middle"
+                                        style="font-family:Arial, Helvetica, sans-serif; color:#373737; font-size:14px;">
+                                        &nbsp;</td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="top"><a
+                                                href="http://actualmanager.com/click.php?aspid=515512876488c17dccba70be83414ab7"
+                                                target="_blank"><img
+                                                    src="http://cotizar-seguro-auto.com/newsletters/19/images/aig-04.jpg"
+                                                    width="100%" style="max-width:57px; display:block; border:0;"
+                                                    alt="Servicio de gr&uacute;a"/></a></td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="middle"
+                                        style="font-family:Arial, Helvetica, sans-serif; color:#373737; font-size:14px;">
+                                        <strong style=" color:#00a4e4; font-size:17px;">Servicio de gr&uacute;a</strong><br>
+                                        Por aver&iacute;a o accidente
+                                    </td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="top"><a
+                                                href="http://actualmanager.com/click.php?aspid=515512876488c17dccba70be83414ab7"
+                                                target="_blank"><img
+                                                    src="http://cotizar-seguro-auto.com/newsletters/19/images/aig-07.jpg"
+                                                    width="100%" style="max-width:57px; display:block; border:0;"
+                                                    alt="Seguimiento en l&iacute;nea"/></a></td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                    <td align="left" valign="middle"
+                                        style="font-family:Arial, Helvetica, sans-serif; color:#373737; font-size:14px;">
+                                        <strong style=" color:#00a4e4; font-size:17px;">Seguimiento en
+                                            l&iacute;nea</strong><br>
+                                        de la reparaci&oacute;n de tu veh&iacute;culo
+                                    </td>
+                                    <td align="left" valign="top">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="left" valign="top" height="50">&nbsp;</td>
+                    </tr>
+
+                    <tr>
+                        <td align="left" valign="top">
+                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                <tr>
+                                    <td align="right" valign="top" width="25%"><img
+                                                src="http://cotizar-seguro-auto.com/newsletters/19/images/aig-08.jpg"
+                                                width="100%" style="max-width:81px; display:block; border:0;"
+                                                alt="flecha"/>
+                                    </td>
+                                    <td align="center" valign="top">
+                                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                            <tr>
+                                                <td style="background-color:#e77b1c;" height="58" align="center"
+                                                    valign="middle"><a
+                                                            href="http://actualmanager.com/click.php?aspid=515512876488c17dccba70be83414ab7"
+                                                            target="_blank"
+                                                            style=" background-color:#e77b1c; font-family:Arial, Helvetica, sans-serif; font-size:24px; color:#f9f9f9; display:block; text-decoration:none; line-height:44px;"><strong>Cotiza
+                                                            aqu&iacute; &raquo;</strong></a></td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                    <td align="left" valign="top" width="25%">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td height="70" align="left" valign="top">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td align="left" valign="top">
+                            <table style="background-color:#FFF;" width="100%" border="0" cellspacing="0"
+                                   cellpadding="0">
+                                <tr>
+                                    <td width="3%">&nbsp;</td>
+                                    <td align="center" valign="middle"
+                                        style="font-family:Arial, Helvetica, sans-serif; font-size:10px; color:#959595;">
+                                        *Plazo m&aacute;ximo en p&eacute;rdida total a partir de la entrega de la
+                                        documentaci&oacute;n completa.<br>
+                                        Producto suscrito por AIG Seguros M&eacute;xico SA de CV. Registro del 03 de
+                                        octubre
+                                        de 2012 &ndash; CNSF-S0012-0439-2012. <br>
+                                        Aplican restricciones, t&eacute;rminos y condiciones del producto contratado.
+                                    </td>
+                                    <td width="3%">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="left" valign="top">&nbsp;</td>
+                    </tr>
+                    <tr height="25">
+                        <td bgcolor="#eaeaea" align="center">
+                            <p style="text-align: center; padding: 10px; color: darkgray;">Si ya no deseas recibir información haz
+                                {{--<a href="{!! route('unSubscribe') !!}/{!! $data['email'] !!}" style="text-decoration: none">clic--}}
+                                    {{--aquí.</a>--}}
+                            </p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</table>
+
+</body>
+</html>
